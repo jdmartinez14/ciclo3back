fd57895ab856ffbdf420ad667f5723902f773e8d
Joseph Martinez <jdmartinez14>
2021-10-11 11:13:35 -0500

    Commit inicial del Backend con la modificación del archivo propierties para la conexión a base de datos y la creación del modelo, api y dao.

---
+++ src/main/java/co/edu/unbosque/ciclo3back/model/Usuarios.java
@@ -0,0 +1,49 @@
+package co.edu.unbosque.ciclo3back.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity // Declara la clase como una entidad conectada a una tabla equivalente en la base de datos
+public class Usuarios {
+	
+	@Id // Indica que el atributo siguiente es la llave primaria definida en la tabla
+	private long cedula_usuario;
+	private String nombre_usuario;
+	private String email_usuario;
+	private String usuario;
+	private String password;
+	
+	// Getters and Setters
+	
+	public long getCedula_usuario() {
+		return cedula_usuario;
+	}
+	public void setCedula_usuario(long cedula_usuario) {
+		this.cedula_usuario = cedula_usuario;
+	}
+	public String getNombre_usuario() {
+		return nombre_usuario;
+	}
+	public void setNombre_usuario(String nombre_usuario) {
+		this.nombre_usuario = nombre_usuario;
+	}
+	public String getEmail_usuario() {
+		return email_usuario;
+	}
+	public void setEmail_usuario(String email_usuario) {
+		this.email_usuario = email_usuario;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
